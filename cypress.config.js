db6917d246237ed784d224a864e596cb03547b8e
Macarena <maca.vn94@gmail.com>
2024-04-29 18:52:54 -0300

    final d2

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
+    watchForFileChanges: false,
+    baseUrl: 'https://pushing-it.vercel.app/',
+    defaultCommandTimeout: 15000,
   },
 });
---
+++ cypress/e2e/Desafio2.cy.js
@@ -0,0 +1,78 @@
+
+describe ('Desafio2' , () => {
+    let data;
+    before('Before' , () => { 
+        cy.fixture("datos").then (dato => {
+            data = dato });
+
+
+        });
+
+    beforeEach('BeforeEach' , () => {
+        cy.visit('');
+        cy.get('#registertoggle').dblclick();
+        cy.get('#user').type(data.login.username);
+        cy.get('#pass').type(data.login.password);
+        cy.get('button[type="submit"]').click();
+        cy.get('#todolistlink').click();
+        cy.wait(2000);
+        cy.get('#removeAll').click();
+        cy.wait(3000)
+     })
+
+    it('1.Ingresar las tareas' , ()=> {
+        cy.get('#task').type(data.tareas.tarea1);
+        cy.get('#sendTask').click();
+        cy.wait(2000)
+        cy.get('#task').type(data.tareas.tarea2);
+        cy.get('#sendTask').click();
+        cy.contains('p', data.tareas.tarea2);
+        cy.get('#task').type(data.tareas.tarea3);
+        cy.get('#sendTask').click();
+        cy.contains('p', data.tareas.tarea3);
+        cy.get('#task').type(data.tareas.tarea4);
+        cy.get('#sendTask').click();
+        cy.wait(2000)
+        cy.get('#task').type(data.tareas.tarea5);
+        cy.get('#sendTask').click();
+        cy.wait(2000)
+    });
+
+    it('2.Verificar botones' , ()=> {
+        cy.get('#all').should('exist');
+        cy.get('#completed').should('exist');
+        cy.get('#active').should('exist');
+        cy.get('#removeAll').should('exist');
+
+    });
+
+    it('3.Agregar y borrar tarea2' , ()=> {
+        cy.get('#task').type(data.tareas.tarea1);
+        cy.get('#sendTask').click();
+        cy.wait(2000)
+        cy.get('#task').type(data.tareas.tarea2);
+        cy.get('#sendTask').click();
+        cy.wait(2000)
+        cy.contains('p', data.tareas.tarea2).siblings('button').click();
+        cy.get('#removeAll').click();
+        cy.wait(3000)
+
+
+    });
+
+    it('4.Agregar y borrar tarea1' , ()=> {
+        cy.get('#task').type(data.tareas.tarea1);
+        cy.get('#sendTask').click();
+        cy.wait(2000)
+        cy.get('#task').type(data.tareas.tarea2);
+        cy.get('#sendTask').click();
+        cy.wait(2000)
+        cy.contains('p', data.tareas.tarea1).siblings('button').click();
+
+        
+
+    });
+
+    
+
+})
